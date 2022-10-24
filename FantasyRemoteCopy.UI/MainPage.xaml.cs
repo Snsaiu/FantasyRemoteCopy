8dc4b812f7502d3120d4b3f33c769d19c16f963a
zhulinshu <zhulinshu@qq.com>
2022-10-24 17:06:29 +0800

    暂存

--- FantasyRemoteCopy.UI/MainPage.xaml.cs
+++ FantasyRemoteCopy.UI/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+
 using FantasyRemoteCopy.Core;
 using FantasyRemoteCopy.Core.Impls;
 
@@ -15,12 +17,35 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-	
+    public void GetAllMacAddressesAndIppairs()
+    {
+      
+        System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+        pProcess.StartInfo.FileName = "arp";
+        pProcess.StartInfo.Arguments = "-a ";
+        pProcess.StartInfo.UseShellExecute = false;
+        pProcess.StartInfo.RedirectStandardOutput = true;
+        pProcess.StartInfo.CreateNoWindow = true;
+        pProcess.Start();
+        string cmdOutput = pProcess.StandardOutput.ReadToEnd();
+        string pattern = @"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}-?){6})";
 
-	private async void OnCounterClicked(object sender, EventArgs e)
+        foreach (Match m in Regex.Matches(cmdOutput, pattern, RegexOptions.IgnoreCase))
+        {
+
+            this.info.Text += m.Groups["ip"].Value+"\n";
+         
+        }
+
+       
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
     {
 
+        this.GetAllMacAddressesAndIppairs();
 
+        return;
         this.info.Text = DateTime.Now.ToString();
 
         IScanLocalNetIp scanLocalNetIp = new DefaultScanLocalNetIp(new DefaultLocalIp());
