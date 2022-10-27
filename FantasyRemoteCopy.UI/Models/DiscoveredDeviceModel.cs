2dec214653207c39477faa7505b89194409df102
zhulinshu <zhulinshu@qq.com>
2022-10-27 17:40:41 +0800

    完成homepage

---
+++ FantasyRemoteCopy.UI/Models/DiscoveredDeviceModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyRemoteCopy.UI.Models
+{
+    /// <summary>
+    /// 设备发现的模型
+    /// </summary>
+    [ObservableObject]
+    public partial class DiscoveredDeviceModel
+    {
+
+        [ObservableProperty]
+        private string deviceType;
+
+        [ObservableProperty]
+        private string nickName;
+
+        [ObservableProperty]
+        private string ip;
+
+        [ObservableProperty]
+        private ImageSource img;
+
+        [ObservableProperty]
+        private string deviceName;
+
+    }
+}
--- FantasyRemoteCopy.UI/ViewModels/HomePageModel.cs
+++ FantasyRemoteCopy.UI/ViewModels/HomePageModel.cs
@@ -3,10 +3,15 @@ using CommunityToolkit.Mvvm.Input;
 
 using FantasyRemoteCopy.Core;
 using FantasyRemoteCopy.Core.Bussiness;
+using FantasyRemoteCopy.Core.Models;
+using FantasyRemoteCopy.UI.Models;
 using FantasyRemoteCopy.UI.Views;
+
+using Newtonsoft.Json;
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +22,42 @@ namespace FantasyRemoteCopy.UI.ViewModels
     public partial class HomePageModel
     {
 
-        public HomePageModel(IUserService userService, SendDataBussiness sendDataBussiness) {
+        public HomePageModel(IUserService userService, SendDataBussiness sendDataBussiness,ReceiveBussiness receiveBussiness) {
             this.userService = userService;
             this.sendDataBussiness = sendDataBussiness;
+            this.receiveBussiness = receiveBussiness;
+
+            this.DiscoveredDevices = new ObservableCollection<DiscoveredDeviceModel>();
+            
+            //发现可用的设备回调
+            this.receiveBussiness.DiscoverEnableIpEvent += (model) =>
+            {
+                DiscoveredDeviceModel ddm = new DiscoveredDeviceModel();
+                ddm.DeviceType = model.DevicePlatform;
+                ddm.DeviceName=model.DeviceName;
+                ddm.NickName = model.NickName;
+                ddm.Ip = model.DeviceIP;
+
+                if (ddm.DeviceType == "WinUI")
+                    ddm.Img = ImageSource.FromFile("windows.png");
+                if (ddm.DeviceType == "MacCatalyst")
+                    ddm.Img = ImageSource.FromFile("mac.png");
+
+                this.DiscoveredDevices.Add(ddm);
+
+
+
+            };
+
+            this.receiveBussiness.ReceiveDataEvent += (d) =>
+            {
+                var str = Encoding.UTF8.GetString(d.Data);
+                var dm = JsonConvert.DeserializeObject<DataMetaModel>(str);
+
+
+            };
+
+
         }
 
         [ObservableProperty]
@@ -29,9 +67,13 @@ namespace FantasyRemoteCopy.UI.ViewModels
         private string userName;
 
         [ObservableProperty]
+        private ObservableCollection<DiscoveredDeviceModel> discoveredDevices;
+
+        [ObservableProperty]
         private string deviceNickName;
         private readonly IUserService userService;
         private readonly SendDataBussiness sendDataBussiness;
+        private readonly ReceiveBussiness receiveBussiness;
 
         [ICommand]
         public async void Init()
@@ -62,6 +104,7 @@ namespace FantasyRemoteCopy.UI.ViewModels
         /// </summary>
         private  void deviceDiscover()
         {
+            this.DiscoveredDevices.Clear();
             this.IsBusy= true;
             Task.Run(async () =>
             {
