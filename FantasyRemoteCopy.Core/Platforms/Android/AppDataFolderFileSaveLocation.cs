8786f34de46aeba975e1a3809c3b2a593ee7b300
Saiu <34592712+Snsaiu@users.noreply.github.com>
2022-10-30 20:32:27 +0800

    添加安卓，但是没有实现必要的功能

---
+++ FantasyRemoteCopy.Core/Platforms/Android/AppDataFolderFileSaveLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace FantasyRemoteCopy.Core.Platforms;
+
+public class AppDataFolderFileSaveLocation: IFileSaveLocation
+{
+    public string GetSaveLocation()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FantasyRemoteCopy.Core/Platforms/Android/DefaultOpenFolder.cs
@@ -0,0 +1,9 @@
+﻿namespace FantasyRemoteCopy.Core.Platforms;
+
+public class DefaultOpenFolder: IOpenFolder
+{
+    public void OpenFolder(string path)
+    {
+        throw new NotImplementedException();
+    }
+}
