2479afa1f7f7e84e96c0a56893089039631df5fe
方留军 <liujun.fang@fa-software.com>
2024-09-23 15:25:45 +0800

    优化查找设备的算法

--- FantasyRemoteCopy.UI/Interfaces/Impls/LocalIpScannerBase.cs
+++ FantasyRemoteCopy.UI/Interfaces/Impls/LocalIpScannerBase.cs
@@ -1,6 +1,7 @@
 using FantasyRemoteCopy.Core.Enums;
 using FantasyRemoteCopy.UI.Models;
 
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
@@ -10,10 +11,21 @@ namespace FantasyRemoteCopy.UI.Interfaces.Impls;
 
 public class LocalIpScannerBase : IGetLocalNetDevices
 {
-    protected virtual string Pattern { get; }=@"(?<ip>([0-9]{1,3}\.?){4})\s*\) at ([0-9]|[a-z])";
-    
+    private readonly DeviceLocalIpBase _deviceLocalIpBase;
+    protected virtual string Pattern { get; } = @"(?<ip>([0-9]{1,3}\.?){4})\s*\) at ([0-9]|[a-z])";
+
+    public LocalIpScannerBase(DeviceLocalIpBase deviceLocalIpBase)
+    {
+        _deviceLocalIpBase = deviceLocalIpBase;
+    }
+
     public async IAsyncEnumerable<ScanDevice> GetDevicesAsync([EnumeratorCancellation] CancellationToken cancellationToken)
     {
+        string localIp = await _deviceLocalIpBase.GetLocalIpAsync();
+
+        List<string> list = localIp.Split(".").ToList()[0..2];
+        string ipStart = string.Join(".", list);
+
         System.Collections.ObjectModel.ReadOnlyCollection<string> scanIps = await Task.Run(() =>
         {
             List<string> temp = [];
@@ -25,7 +37,7 @@ public class LocalIpScannerBase : IGetLocalNetDevices
             pProcess.StartInfo.CreateNoWindow = true;
             pProcess.Start();
             string cmdOutput = pProcess.StandardOutput.ReadToEnd();
-         
+
             foreach (Match m in Regex.Matches(cmdOutput, Pattern, RegexOptions.IgnoreCase))
             {
                 temp.Add(m.Groups["ip"].Value);
@@ -38,9 +50,35 @@ public class LocalIpScannerBase : IGetLocalNetDevices
             yield break;
         foreach (string item in scanIps)
         {
-            if (item.StartsWith("192.168"))
-                yield return new ScanDevice(SystemType.None, Device.None,item, null);
+            if (item.StartsWith(ipStart))
+                yield return new ScanDevice(SystemType.None, Device.None, item, null);
         }
 
     }
+
+
+    private static IPAddress GetLowestIp(IPAddress address, IPAddress mask)
+    {
+        byte[] addressBytes = address.GetAddressBytes();
+        byte[] maskBytes = mask.GetAddressBytes();
+        if (addressBytes.Length != 4 || maskBytes.Length != 4)
+            return IPAddress.None;
+        byte[] lowest = new byte[4];
+        for (int i = 0; i < 4; i++)
+            lowest[i] = (byte)(addressBytes[i] & maskBytes[i]);
+        return new IPAddress(lowest);
+    }
+
+    private static IPAddress GetHighestIp(IPAddress address, IPAddress mask)
+    {
+        byte[] addressBytes = address.GetAddressBytes();
+        byte[] maskBytes = mask.GetAddressBytes();
+        if (addressBytes.Length != 4 || maskBytes.Length != 4)
+            return IPAddress.None;
+        byte[] highest = new byte[4];
+        for (int i = 0; i < 4; i++)
+            highest[i] = (byte)((addressBytes[i] & maskBytes[i]) | ~maskBytes[i]);
+        return new IPAddress(highest);
+    }
+
 }
--- FantasyRemoteCopy.UI/Platforms/Windows/DefaultScanLocalNetIp.cs
+++ FantasyRemoteCopy.UI/Platforms/Windows/DefaultScanLocalNetIp.cs
@@ -2,9 +2,12 @@
 
 namespace FantasyRemoteCopy.UI;
 
-public class DefaultScanLocalNetIp:LocalIpScannerBase
+public class DefaultScanLocalNetIp : LocalIpScannerBase
 {
+    public DefaultScanLocalNetIp(DeviceLocalIpBase deviceLocalIpBase) : base(deviceLocalIpBase)
+    {
+    }
 
-   protected override string Pattern { get; }=@"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}-?){6})";
+    protected override string Pattern { get; } = @"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}-?){6})";
 }
 
