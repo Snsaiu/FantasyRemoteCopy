d9732965688ee99678a0f60c4120b11a51e5d53c
方留军 <snsaiu@outlook.com>
2024-10-05 10:49:57 +0800

    修复错误

--- FantasyRemoteCopy.UI/Platforms/Windows/LocalNetDeviceDiscovery.cs
+++ FantasyRemoteCopy.UI/Platforms/Windows/LocalNetDeviceDiscovery.cs
@@ -1,7 +1,9 @@
 using FantasyRemoteCopy.UI.Interfaces.Impls;
 namespace FantasyRemoteCopy.UI;
 
-public sealed class LocalNetDeviceDiscovery:LocalNetDeviceDiscoveryBase
+public sealed class LocalNetDeviceDiscovery : LocalNetDeviceDiscoveryBase
 {
-
+    public LocalNetDeviceDiscovery(DeviceLocalIpBase localIpBase) : base(localIpBase)
+    {
+    }
 }
--- FantasyRemoteCopy.UI/Platforms/Windows/LocalNetJoinProcess.cs
+++ FantasyRemoteCopy.UI/Platforms/Windows/LocalNetJoinProcess.cs
@@ -2,7 +2,9 @@ using FantasyRemoteCopy.UI.Interfaces.Impls;
 
 namespace FantasyRemoteCopy.UI;
 
-public sealed class LocalNetJoinProcess:LocalNetJoinProcessBase
+public sealed class LocalNetJoinProcess : LocalNetJoinProcessBase
 {
-
+    public LocalNetJoinProcess(DeviceLocalIpBase localIpBase) : base(localIpBase)
+    {
+    }
 }
