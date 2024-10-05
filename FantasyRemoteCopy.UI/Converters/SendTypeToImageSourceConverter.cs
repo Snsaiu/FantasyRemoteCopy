1edc3cccef0ccb39691dc6861f6ec7ae31697d6c
Saiu <SNsaiu@outlook.com>
2024-10-05 16:38:35 +0800

    优化样式

---
+++ FantasyRemoteCopy.UI/Converters/SendTypeToImageSourceConverter.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using FantasyRemoteCopy.UI.Enums;
+
+namespace FantasyRemoteCopy.UI.Converters;
+
+public sealed class SendTypeToImageSourceConverter:ValueConverterBase
+{
+    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not SendType type)
+        {
+            return Binding.DoNothing;
+        }
+        
+        return type switch
+        {
+            SendType.Text => ImageSource.FromFile("texticon.png"),
+            SendType.File => ImageSource.FromFile("fileicon.png"),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
