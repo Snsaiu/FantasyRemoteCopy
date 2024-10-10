79750d7ad55c325c886b58f7142b5ebbbf104c6a
Saiu <SNsaiu@outlook.com>
2024-10-10 14:58:01 +0800

    获得本地ip添加描述限定判断，排除虚拟网卡 (#20)
    * 获得本地ip添加描述限定判断，排除虚拟网卡
    
    * 修复ip判定错误
    
    ---------
    
    Co-authored-by: 方留军 <liujun.fang@fa-software.com>

--- FantasyRemoteCopy.UI/Interfaces/Impls/DeviceLocalIpBase.cs
+++ FantasyRemoteCopy.UI/Interfaces/Impls/DeviceLocalIpBase.cs
@@ -4,7 +4,7 @@ using System.Net.Sockets;
 namespace FantasyRemoteCopy.UI.Interfaces.Impls;
 
 /// <summary>
-/// 设备获得本机ip
+///     设备获得本机ip
 /// </summary>
 public abstract class DeviceLocalIpBase : IGetLocalIp
 {
@@ -12,7 +12,10 @@ public abstract class DeviceLocalIpBase : IGetLocalIp
     {
         var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces()
             .Where(nic => nic.NetworkInterfaceType is NetworkInterfaceType.Wireless80211 or
-                          NetworkInterfaceType.Ethernet);
+                    NetworkInterfaceType.Ethernet && !nic.Description.Contains("VMware")
+                                                  && !nic.Description.Contains("Virtual")
+                                                  && !nic.Description.Contains("VPN")
+                                                  && !nic.Description.Contains("Ethernet"));
 
         foreach (var nic in networkInterfaces)
         {
@@ -22,10 +25,7 @@ public abstract class DeviceLocalIpBase : IGetLocalIp
 
             if (ipAddress == null) continue;
 
-            if (nic.OperationalStatus == OperationalStatus.Up)
-            {
-                return Task.FromResult(ipAddress.Address.ToString());
-            }
+            if (nic.OperationalStatus == OperationalStatus.Up) return Task.FromResult(ipAddress.Address.ToString());
         }
 
         throw new NullReferenceException("无法找到本机ip");
