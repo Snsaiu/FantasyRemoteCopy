1895da5ec2378f7590f7fd6d77af36f02b817d38
方留军 <liujun.fang@fa-software.com>
2024-10-12 18:22:25 +0800

    添加发送接口服务

--- FantasyRemoteCopy.UI/Interfaces/Impls/TcpTransfer/TcpLoopListenerBase.cs
+++ FantasyRemoteCopy.UI/Interfaces/Impls/TcpTransfer/TcpLoopListenerBase.cs
@@ -1,10 +1,12 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using FantasyRemoteCopy.UI.Consts;
+﻿using FantasyRemoteCopy.UI.Consts;
 using FantasyRemoteCopy.UI.Interfaces.Impls.Configs;
 using FantasyRemoteCopy.UI.Models;
+
 using Newtonsoft.Json;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace FantasyRemoteCopy.UI.Interfaces.Impls.TcpTransfer;
 
@@ -12,24 +14,30 @@ namespace FantasyRemoteCopy.UI.Interfaces.Impls.TcpTransfer;
 public abstract class LoopListenerBase<T, P, R> : IReceiveableWithProgress<T, P>
     where T : TransformResultModel<R> where P : IProgressValue
 {
-    protected virtual Task OnCancelReceiveAsync() => Task.CompletedTask;
-    
+    protected virtual Task OnCancelReceiveAsync()
+    {
+        return Task.CompletedTask;
+    }
+
     public abstract Task ReceiveAsync(Action<T> receivedCallBack, IProgress<P>? progress,
         CancellationToken cancellationToken);
 }
 
 
-public abstract class HttpsLoopListenContentBase(FileSavePathBase fileSavePathBase)
+public abstract class HttpsLoopListenContentBase(FileSavePathBase fileSavePathBase, ISendPortService sendPortService)
     : LoopListenerBase<TransformResultModel<string>, ProgressValueModel, string>
 {
+    protected readonly FileSavePathBase FileSavePathBase = fileSavePathBase;
+    protected readonly ISendPortService SendPortService = sendPortService;
+
     public override Task ReceiveAsync(Action<TransformResultModel<string>> receivedCallBack, IProgress<ProgressValueModel>? progress, CancellationToken cancellationToken)
     {
-        
+
     }
 }
 
 
-public abstract class TcpLoopListenerBase<T, P, R> : LoopListenerBase<T, P,R>
+public abstract class TcpLoopListenerBase<T, P, R> : LoopListenerBase<T, P, R>
     where T : TransformResultModel<R> where P : IProgressValue
 {
     public override async Task ReceiveAsync(Action<T> receivedCallBack, IProgress<P>? progress, CancellationToken cancellationToken)
@@ -42,7 +50,7 @@ public abstract class TcpLoopListenerBase<T, P, R> : LoopListenerBase<T, P,R>
             try
             {
                 cancellationToken.ThrowIfCancellationRequested();
-                var client = await listener.AcceptTcpClientAsync(cancellationToken);
+                TcpClient client = await listener.AcceptTcpClientAsync(cancellationToken);
                 _ = HandleClientAsync(client, receivedCallBack, progress, cancellationToken); // 处理客户端连接
             }
             catch (OperationCanceledException)
@@ -61,8 +69,8 @@ public abstract class TcpLoopListenerBase<T, P, R> : LoopListenerBase<T, P,R>
 
     private async Task<string> ReceiveMetadataStringAsync(NetworkStream stream, CancellationToken cancellationToken)
     {
-        var buffer = new byte[200];
-         _ = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
+        byte[] buffer = new byte[200];
+        _ = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
 
         // 2. 读取前 4 字节的原始长度信息
         int originalLength = BitConverter.ToInt32(buffer, 0);
@@ -79,9 +87,9 @@ public abstract class TcpLoopListenerBase<T, P, R> : LoopListenerBase<T, P,R>
 
     private async Task HandleClientAsync(TcpClient client, Action<T> receivedCallBack, IProgress<P>? progress, CancellationToken cancellationToken)
     {
-        var stream = client.GetStream();
-        var metaString = await ReceiveMetadataStringAsync(stream, cancellationToken);
-        var metaMessage = JsonConvert.DeserializeObject<SendMetadataMessage>(metaString);
+        NetworkStream stream = client.GetStream();
+        string metaString = await ReceiveMetadataStringAsync(stream, cancellationToken);
+        SendMetadataMessage? metaMessage = JsonConvert.DeserializeObject<SendMetadataMessage>(metaString);
         if (metaMessage is null)
         {
             throw new NullReferenceException();
