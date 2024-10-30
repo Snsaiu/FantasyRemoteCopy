7cbc6a018576037f1e0490fbf877a88f951dc6f4
方留军 <liujun.fang@fa-software.com>
2024-10-30 13:37:48 +0800

    测试成功，需要关闭防火墙

--- HttpReceive/Program.cs
+++ HttpReceive/Program.cs
@@ -1,39 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
-string url = "http://localhost:5001/text/";
-HttpListener listener = new HttpListener();
-listener.Prefixes.Add(url);
+// 调用此方法启动服务器
+await HttpServer.StartServer();
 
-
-listener.Start();
-Console.WriteLine($"HTTPS Server is listening at {url}");
-
-while (true)
+internal class HttpServer
 {
-    var context = await listener.GetContextAsync();
-    HttpListenerRequest request = context.Request;
-    HttpListenerResponse response = context.Response;
-
-    if (request.HttpMethod == "POST" && request.Url.AbsolutePath == "/text")
+    public static async Task StartServer()
     {
-        using (StreamReader reader = new StreamReader(request.InputStream, Encoding.UTF8))
+        var listener = new HttpListener();
+        listener.Prefixes.Add("http://10.10.26.157:5001/"); // 使用局域网IP地址作为监听地址
+        listener.Start();
+        Console.WriteLine("服务器已启动，等待连接...");
+
+        while (true)
         {
-            string requestData = await reader.ReadToEndAsync();
-            Console.WriteLine($"Received text data: {requestData}");
-        }
+            var context = await listener.GetContextAsync();
+            var request = context.Request;
 
-        string responseString = "Text received successfully!";
-        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-        response.ContentLength64 = buffer.Length;
-        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
-    }
-    else
-    {
-        response.StatusCode = (int)(request.HttpMethod != "POST" ? HttpStatusCode.MethodNotAllowed : HttpStatusCode.NotFound);
-        response.Close();
+            // 读取客户端发送的数据
+            string requestData;
+            using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                requestData = await reader.ReadToEndAsync();
+            }
+
+            Console.WriteLine("收到请求数据: " + requestData);
+
+            // 发送响应数据
+            var response = context.Response;
+            var responseString = "Hello from the server!";
+            var buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
+        }
     }
 }
