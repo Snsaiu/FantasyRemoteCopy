2ef896f20d44af1d384c8a39f0d872a8ba2c8dad
方留军 <liujun.fang@fa-software.com>
2024-11-04 16:13:26 +0800

    icon 初步可以显示

---
+++ Fantasy.UIKit/Interfaces/IForegroundColorElement.cs
@@ -0,0 +1,13 @@
+﻿namespace Fantasy.UIKit.Interfaces;
+
+/// <summary>
+///     可绘制前景色
+/// </summary>
+internal interface IForegroundColorElement : IUIKitElement
+{
+    public static readonly BindableProperty ForegroundColorProperty = BindableProperty.Create(nameof(ForegroundColor),
+        typeof(Color), typeof(IForegroundColorElement), Colors.Black,
+        propertyChanged: (bindable, obj, v) => ((IUIKitElement)bindable).OnPropertyChanged());
+
+    public Color ForegroundColor { get; set; }
+}
