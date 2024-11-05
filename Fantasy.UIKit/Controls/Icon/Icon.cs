60ff3eab5a1f7e3be81317ce0ad5475204f6aebf
方留军 <liujun.fang@fa-software.com>
2024-11-05 14:12:41 +0800

    添加icon

---
+++ Fantasy.UIKit/Controls/Icon/Icon.cs
@@ -0,0 +1,32 @@
+﻿using Fantasy.UIKit.Converters;
+using Fantasy.UIKit.Interfaces;
+using Fantasy.UIKit.Primitives;
+
+using System.ComponentModel;
+
+namespace Fantasy.UIKit.Controls.Icon;
+
+public class Icon : InterfaceGraphicsView, IIconElement
+{
+    public Icon()
+    {
+        IconDrawable drawable = new IconDrawable(this);
+        Drawable = drawable;
+    }
+
+    public static readonly BindableProperty CornerRadiusProperty = ICornerRadiusShapeElement.CornerRadiusProperty;
+
+    [TypeConverter(typeof(CornerRadiusShapeConverter))]
+    public CornerRadiusShape CornerRadius { get => (CornerRadiusShape)GetValue(CornerRadiusProperty); set => SetValue(CornerRadiusProperty, value); }
+
+    public static new readonly BindableProperty BackgroundColorProperty = IBackgroundElement.BackgroundColorProperty;
+    public new Color BackgroundColor { get => (Color)GetValue(BackgroundColorProperty); set => SetValue(BackgroundColorProperty, value); }
+
+    public static readonly BindableProperty ForeregroundColorProperty = IForegroundColorElement.ForegroundColorProperty;
+    public Color ForegroundColor { get => (Color)GetValue(ForeregroundColorProperty); set => SetValue(ForeregroundColorProperty, value); }
+
+    PathF? IIconElement.IconPath { get; set; }
+
+    public static readonly BindableProperty IconDataProperty = IIconElement.IconDataProperty;
+    public string IconData { get => (string)GetValue(IconDataProperty); set => SetValue(IconDataProperty, value); }
+}
---
+++ Fantasy.UIKit/Controls/Icon/IconDrawable.cs
@@ -0,0 +1,19 @@
+﻿using Fantasy.UIKit.Extensions;
+
+namespace Fantasy.UIKit.Controls.Icon;
+
+public class IconDrawable(Icon view) : IDrawable
+{
+    private readonly Icon _view = view;
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.SaveState();
+        canvas.Antialias = true;
+        canvas.ClipPath(_view.GetClipPath(dirtyRect));
+        float scale = dirtyRect.Height / 40f;
+        canvas.DrawIcon(_view, dirtyRect, 24, scale);
+        canvas.ResetState();
+
+    }
+}
