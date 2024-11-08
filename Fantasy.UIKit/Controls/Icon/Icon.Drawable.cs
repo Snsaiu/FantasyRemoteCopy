86f31874dd548d5ce1d1fcafbe5f12305cfb6457
方留军 <liujun.fang@fa-software.com>
2024-11-08 09:14:38 +0800

    添加被忽略的文件

---
+++ Fantasy.UIKit/Controls/Icon/Icon.Drawable.cs
@@ -0,0 +1,16 @@
+﻿namespace Fantasy.UIKit;
+
+public partial class Icon
+{
+    public override void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.SaveState();
+        canvas.Antialias = true;
+        canvas.ClipPath(this.GetClipPath(dirtyRect));
+        canvas.DrawBackground(this, dirtyRect);
+        canvas.DrawBorderColor(this, dirtyRect);
+        var scale = dirtyRect.Height / 40f;
+        canvas.DrawIcon(this, dirtyRect, 24, scale);
+        canvas.ResetState();
+    }
+}
