44ed77077bdf0271488f70d82698ce8cf98ea38d
Saiu <SNsaiu@outlook.com>
2024-12-02 23:00:36 +0800

    添加导航

--- AirTransfer/Components/Pages/Login.razor.cs
+++ AirTransfer/Components/Pages/Login.razor.cs
@@ -9,12 +9,16 @@ public partial class Login : ComponentBase
     [Parameter] public string? DeviceName { get; set; }
 
 
+    [Inject] private NavigationManager NavigationManager { get; set; }
+
     private Task LoginAsync()
     {
         if (string.IsNullOrWhiteSpace(LoginName) || string.IsNullOrWhiteSpace(DeviceName))
         {
             return Task.CompletedTask;
         }
+
+        this.NavigationManager.NavigateTo($"/home");
 
         return Task.CompletedTask;
     }
